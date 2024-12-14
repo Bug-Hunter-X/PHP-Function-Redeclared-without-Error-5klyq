bc337772471602749f22a64a5fe3f35a8062a1bc
BugHunter-X <askzenai@gmail.com>
2024-12-14 22:14:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,13 @@
+function myFunc() {
+  // Some code here
+}
+
+myFunc(); // Call the function
+
+// ...later in the code...
+
+function myFunc() {
+  // Different code here
+}
+
+myFunc(); // Call the function again
