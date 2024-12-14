8ce4300474cbace8e0b73479fa1bd0e969b6a4a6
BugHunter-X <askzenai@gmail.com>
2024-12-14 22:14:05 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,10 @@
+//This version prevents the function redeclaration error by using a single definition of myFunc. 
+function myFunc(){
+// function body here
+}
+
+myFunc(); // Call the function 
+
+// ...later in the code...
+
+myFunc(); // Call the function again
